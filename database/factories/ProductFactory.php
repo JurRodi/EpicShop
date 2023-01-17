f3f15cc3ca344a87ed0134828009ce1d48493388
JurRodi <rodijurrien@gmail.com>
2023-01-17 23:45:44 +0100

    Feat: add product factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'price' => $this->faker->numberBetween(20, 60),
+            'description' => $this->faker->text,
+            'category' => $this->faker->word,
+            'image' => $this->faker->imageUrl(),
+        ];
+    }
+}
