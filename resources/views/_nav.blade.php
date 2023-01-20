a578240f9f9886d95751177c3fd25725caccf07d
Cournflaeik <elien.degeetere@gmail.com>
2023-01-20 06:04:14 +0100

    feat: add nav component

---
+++ resources/views/_nav.blade.php
@@ -0,0 +1,11 @@
+<nav class="d-flex align-items-center" style="height:5rem; padding:3rem">
+    <img src="plush_logo.jpg" alt="Logo Plush" style="height:2rem">
+    <ul class="nav ms-auto">
+        <li class="nav-item">
+            <a class="nav-link info font-monospace" style="color: deepskyblue !important" aria-current="page" href="/products">Products</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link font-monospace" style="color: deepskyblue !important" href="/admin">Admin</a>
+        </li>
+    </ul>
+</nav>
