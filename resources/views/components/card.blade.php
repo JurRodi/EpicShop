46c782b17ac470b74e66ee8c1dba768a6d3a55ef
Cournflaeik <elien.degeetere@gmail.com>
2023-01-20 14:51:51 +0100

    Style: card styling

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,12 +1,12 @@
-<a href="/products/{{ $id }}">
+<a href="/products/{{ $id }}" class="text-decoration-none">
     <div class="col-md-4">
         <div class="card mb-4 shadow-sm" style="height:30rem">
             <img class="card-img-top" src="{{ $image }}" alt="Card image cap">
             <div class="card-body">
-                <h5 class="card-title">{{ $name }}</h5>
-                <p class="card-text">{{ $description }}</p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <small class="text-muted">{{ $price }}</small>
+                <h5 class="card-title text-info">{{ $name }}</h5>
+                <p class="card-text text-body">{{ $description }}</p>
+                <div class="container d-flex justify-content-between align-items-center bg-danger rounded py-1 px-3">
+                    <small class="text-light">{{ $price }}</small>
                 </div>
                 <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
