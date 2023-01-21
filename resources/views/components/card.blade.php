48803bbac00885362756679bd56d485b9680e255
Cournflaeik <elien.degeetere@gmail.com>
2023-01-21 18:39:28 +0100

    style: add more card styling

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,31 +1,33 @@
 <a href="/products/{{ $id }}" class="text-decoration-none">
     <div class="col-md-4">
-        <div class="card mb-4 shadow-sm" style="height:30rem">
+        <div class="card mb-4 shadow-sm" style="height:38rem">
             <img class="card-img-top" src="{{ $image }}" alt="Card image cap">
             <div class="card-body">
                 <h5 class="card-title text-info">{{ $name }}</h5>
                 <p class="card-text text-body">{{ $description }}</p>
-                <div class="container d-flex justify-content-between align-items-center bg-danger rounded py-1 px-3">
-                    <small class="text-light">{{ $price }}</small>
+                <div class="buttons d-flex justify-content-between mt-3">
+                    <div class="container d-flex justify-content-between align-items-center bg-danger rounded py-1 px-3" style="width:3rem">
+                        <small class="text-light">{{ $price }}</small>
+                    </div>
+                    @if(Auth::check())
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/products/edit/{{ $id }}" class="btn btn-sm btn-outline-secondary">Edit</a>
+                                <form action="/products/delete/{{ $id }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-sm btn-outline-secondary">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    @else
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="/products/add-to-cart/{{ $id }}" class="btn btn-sm btn-outline-secondary">add to cart</a>
+                            </div>
+                        </div>
+                    @endif
                 </div>
-                @if(Auth::check())
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="/products/edit/{{ $id }}" class="btn btn-sm btn-outline-secondary">Edit</a>
-                            <form action="/products/delete/{{ $id }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-sm btn-outline-secondary">Delete</button>
-                            </form>
-                        </div>
-                    </div>
-                @else
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                            <a href="/products/add-to-cart/{{ $id }}" class="btn btn-sm btn-outline-secondary">add to cart</a>
-                        </div>
-                    </div>
-                @endif
             </div>
         </div>
     </div>
