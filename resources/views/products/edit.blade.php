cc6b5ab9c258bcd81e781027c100fe893a6106eb
Cournflaeik <elien.degeetere@gmail.com>
2023-01-22 01:28:42 +0100

    Feat: add edit page

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts/app')
 
 @section('content')
-
+    <div class="card container-sm mb-3 p-0 w-25" >
+        <div class="card-img-top w-100 h-50 bg-light"><img src="{{ $product->image }}" alt="Product image" class="rounded img-fluid h-100 w-100"></div>
+        <div class="card-body">
+            <h5 class="card-title">{{ $product->name }}</h5>
+            <p class="card-text" style="height: 72px">{{ $product->description }}</p>
+        </div>
+    </div>
+@endsection
