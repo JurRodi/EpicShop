9b8db4aef7dffe70c4faa780aa6486d10c2543f9
Cournflaeik <elien.degeetere@gmail.com>
2023-01-22 01:48:52 +0100

    Feat: add form labels to edit page

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -2,10 +2,31 @@
 
 @section('content')
     <div class="card container-sm mb-3 p-0 w-25" >
-        <div class="card-img-top w-100 h-50 bg-light"><img src="{{ $product->image }}" alt="Product image" class="rounded img-fluid h-100 w-100"></div>
+        <div class="card-img-top w-100 h-50 bg-light">
+            <img src="{{ $product->image }}" alt="Product image" class="rounded img-fluid h-100 w-100">
+        </div>
         <div class="card-body">
-            <h5 class="card-title">{{ $product->name }}</h5>
-            <p class="card-text" style="height: 72px">{{ $product->description }}</p>
+            <form method="POST" action="/products/update/{{ $product }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="name" name="name" value="{{ $product->name }}">
+                </div>
+                <div class="mb-3">
+                    <label for="description" class="form-label">Description</label>
+                    <textarea class="form-control" id="description" name="description" rows="3">{{ $product->description }}</textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="price" class="form-label">Price</label>
+                    <input type="number" class="form-control" id="price" name="price" value="{{ $product->price }}">
+                </div>
+                <div class="mb-3">
+                    <label for="image" class="form-label">Image</label>
+                    <input type="file" class="form-control" id="image" name="image">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </div>
     </div>
 @endsection
