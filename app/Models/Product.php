01c29a9164000db588b09454d7caa59aba27e8c4
Cournflaeik <elien.degeetere@gmail.com>
2023-01-23 18:50:15 +0100

    Fix: product order table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProductOrder;
 
 class Product extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'name',
-        'price',
-        'description',
-        'category',
-        'image',
-    ];
+    protected $with = ['orders'];
+    
+    public function orders()
+    {
+        return $this->hasMany(ProductOrder::class);
+    }
 }
