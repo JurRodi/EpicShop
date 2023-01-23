5553429ee45042acbce94e56208ada9f5f7b14d1
JurRodi <rodijurrien@gmail.com>
2023-01-23 20:39:23 +0100

    Fix: get products with their orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,20 +11,16 @@ class OrderController extends Controller
     public function index()
     {
         $orders = DB::table('orders')->get();
-        // $products = DB::table('products')
-        //     ->join('product_orders', 'products.id', '=', 'product_orders.product_id')
-        //     ->join('orders', 'product_orders.order_id', '=', 'orders.id')
-            
-        //     ->get();
-        // select all products in all orders
-        $products = DB::table('products')
+        foreach ($orders as $order) {
+            $order->products = DB::table('products')
             ->join('product_orders', 'products.id', '=', 'product_orders.product_id')
-            ->where('orders', 'product_orders.order_id', '=', 'orders.id')
+            ->join('orders', 'product_orders.order_id', '=', 'orders.id')
             ->get();
+        }
         $data = [
             'orders' => $orders,
-            'products' => $products,
         ];
+        // dd($data);
         return view('admin/orders', $data);
     }
 }
