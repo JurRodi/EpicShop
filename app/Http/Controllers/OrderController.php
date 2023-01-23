b2aef9508e3f5cadcd7740f9cd911cbdf9b6aa09
Cournflaeik <elien.degeetere@gmail.com>
2023-01-23 18:49:00 +0100

    Fix: index function query

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
     public function index()
     {
-        $orders = Order::all();
+        $orders = DB::table('orders')->get();
+        // $products = DB::table('products')
+        //     ->join('product_orders', 'products.id', '=', 'product_orders.product_id')
+        //     ->join('orders', 'product_orders.order_id', '=', 'orders.id')
+            
+        //     ->get();
+        // select all products in all orders
+        $products = DB::table('products')
+            ->join('product_orders', 'products.id', '=', 'product_orders.product_id')
+            ->where('orders', 'product_orders.order_id', '=', 'orders.id')
+            ->get();
         $data = [
             'orders' => $orders,
+            'products' => $products,
         ];
         return view('admin/orders', $data);
     }
