2605875f1d91379aa4896bd3f2072306bf751bdd
JurRodi <rodijurrien@gmail.com>
2023-01-23 20:37:50 +0100

    Feat: add quantity of products

--- database/migrations/2023_01_23_144343_create_product_orders_table.php
+++ database/migrations/2023_01_23_144343_create_product_orders_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('product_orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('quantity');
             $table->foreignId('product_id');
             $table->foreignId('order_id');
             $table->timestamps();
