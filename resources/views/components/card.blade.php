b9f53d2f121b1b0dd2134c68396c884ad82f6857
JurRodi <rodijurrien@gmail.com>
2023-01-23 22:58:00 +0100

    Styling: fix layout

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -23,6 +23,7 @@
                     @else
                         <div class="d-flex justify-content-between align-items-center">
                             <div class="btn-group">
+                                <a></a>
                                 <form action="/cart/add/{{ $id }}" method="POST">
                                     @csrf
                                     <button type="submit" class="btn btn-sm btn-outline-secondary mx-1">Add to cart</button>
