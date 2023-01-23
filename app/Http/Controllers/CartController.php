000e3e4e9c1d63c5574500284b3485f5bae8e1ca
JurRodi <rodijurrien@gmail.com>
2023-01-23 11:17:04 +0100

    Feat: add cart controller

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        $cart = session()->get('cart');
+        $data = [
+            'cart' => $cart,
+        ];
+        return view('users/shoppingCart', $data);
+    }
+
+    public function addToCart()
+    {
+        $product = Product::find($id);
+        if (!$product) {
+            abort(404);
+        }
+        $cart = session()->get('cart');
+        if (!$cart) {
+            $cart = [
+                $id => [
+                    "name" => $product->name,
+                    "quantity" => 1,
+                    "price" => $product->price,
+                    "image" => $product->image
+                ]
+            ];
+            session()->put('cart', $cart);
+            return redirect()->back()->with('success', 'Product added to cart successfully!');
+        }
+        if (isset($cart[$id])) {
+            $cart[$id]['quantity']++;
+            session()->put('cart', $cart);
+            return redirect()->back()->with('success', 'Product added to cart successfully!');
+        }
+        $cart[$id] = [
+            "name" => $product->name,
+            "quantity" => 1,
+            "price" => $product->price,
+            "image" => $product->image
+        ];
+        session()->put('cart', $cart);
+        return redirect()->back()->with('success', 'Product added to cart successfully!');
+    }
+
+    public function order()
+    {
+        $cart = session()->get('cart');
+        $products = Product::find(array_keys($cart));
+        $total = 0;
+        foreach ($products as $product) {
+            $total += $product->price * $cart[$product->id]['quantity'];
+        }
+        return view('users/shoppingCart', $data);
+    }
+}
